0ca85afba6f796a528e857c6952a8c2e05284fe4
Vincent Hisatake <vhisatake@gmail.com>
2021-02-11 13:26:28 -0500

    updated

---
+++ Downloading MySQL Connector/src/main/java/com/caveofprogramming/db/Profile.java
@@ -0,0 +1,29 @@
+package com.caveofprogramming.db;
+
+import java.util.Properties;
+
+public class Profile {
+	
+	public static Properties getProperties(String name) {
+		
+		Properties props = new Properties();
+
+		String env = System.getProperty("env");
+
+		if (env == null) {
+			env = "dev";
+		}
+
+		String propertiesFile = String.format("/config/%s.%s.properties", name, env);
+
+		System.out.println(propertiesFile);
+
+		try {
+			props.load(App.class.getResourceAsStream(propertiesFile));
+		} catch (Exception e) {
+			throw new RuntimeException("cannot load properties file: " + propertiesFile);
+		}
+
+		return props;
+	}
+}
